030f7ead5b35fb43e30ca00c1c6e508153fa1212
HarshadBharade <bharadeharshad106@gmail.com>
2025-04-29 16:37:07 +0530

    this is the ui

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+
+const App = ()=>{
+
+  return (
+    <>
+    <div className='h-screen flex items-center justify-center'>
+      <div className='bg-white rounded-lg p-6 w-96'>
+        <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+          <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center">Create an Account</h2>
+          <form onSubmit={(e)=>{
+            submitHandler(e)
+          }} className="flex flex-col gap-4">
+            <input
+              className="w-full border border-gray-300 px-4 py-2 text-sm rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+              type="text"
+              required
+              name='fullName'
+              placeholder="Enter Name here"
+              
+            />
+            <input
+              className="w-full border border-gray-300 px-4 py-2 text-sm rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+              type="email"
+              required
+              placeholder="Enter Your Email"
+              name='email'
+              
+            />
+            <input
+              className="w-full border border-gray-300 px-4 py-2 text-sm rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+              type="password"
+              required
+              name='password'
+              placeholder="Enter Password"
+              
+            />
+            <input
+            required
+              className="w-full border border-gray-300 px-4 py-2 text-sm rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
+              type="password"
+              name='confirmPassword'
+              placeholder="Confirm Password"
+              
+            />
+
+          
+            
+            <button
+              className="text-sm px-4 py-2 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 w-full mt-3"
+            >
+              Submit
+            </button>
+          </form>
+
+          <p className="text-xs text-gray-600 mt-4 text-center">
+            By registering, you agree to our <span className="text-indigo-600">Terms & Conditions</span> and <span className="text-indigo-600">Privacy Policy</span>.
+          </p>
+        </div>
+        
+      </div>
+
+      
+    </div>
+    
+    </>
+  )
+}
+
+export default App
